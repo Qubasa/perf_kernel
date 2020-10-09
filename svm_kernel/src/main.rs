ea09ba674926c3fb835eae6f41d9e38506361755
Luis Hebendanz <luis.nixos@gmail.com>
2020-10-10 00:34:32 +0200

    Working vga & serial println

--- svm_kernel/src/main.rs
+++ svm_kernel/src/main.rs
@@ -11,55 +11,20 @@
  * family: 0x17h, model: 0x18h
  */
 
-use core::panic::PanicInfo;
-use uart_16550::SerialPort;
-
-static HELLO: &[u8] = b"Hello World!";
+mod print;
+mod serial;
+mod vga;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    let mut serial_port = unsafe { SerialPort::new(0x3F8) };
-    serial_port.init();
-
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2+1) = 0xb;
-        }
-        serial_port.send(byte);
-    };
-
-    // let serial_port_base = 0x3f8 as *mut u8; // Also COM1
-
-    // // Configure
-    // unsafe {
-    //     /*
-    //      *  Configures the line of the given serial port. The port is set to have a
-    //      *  data length of 8 bits, no parity bits, one stop bit and break control
-    //      *  disabled.
-    //      */
-    //     *serial_port_base.offset(3) = 0x03;
-    //     *serial_port_base.offset(2) = 0xC7; // Enable fifo, 14b queue size, clear queues
-    //     *serial_port_base.offset(4) = 0x03; // RTS = 1, DTS = 1
-
-    // }
-
-
-    // for(_i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         while(*serial_port_base.offset(5) & 0x20 == 0){};
-    //         *serial_port_base = byte;
-    //     }
-    // }
+    for i in 0..50{
+        println!("Hello World {}", i);
+    }
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {};
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
-
-
---
+++ svm_kernel/src/print.rs
@@ -0,0 +1,14 @@
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::vga::_print(format_args!($($arg)*));
+        $crate::serial::_print(format_args!($($arg)*))
+    };
+
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
