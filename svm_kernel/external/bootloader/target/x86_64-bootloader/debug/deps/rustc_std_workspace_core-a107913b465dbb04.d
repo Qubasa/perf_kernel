0e5caff7fe6613464dd1b5e32d5758a3ebaccf1b
Luis Hebendanz <luis.nixos@gmail.com>
2020-12-23 21:47:40 +0100

    Replaced gasm bootloader with nasm bootloader

--- svm_kernel/external/bootloader/target/x86_64-bootloader/debug/deps/rustc_std_workspace_core-a107913b465dbb04.d
+++
@@ -1,7 +0,0 @@
-/home/lhebendanz/Projects/rust-kernel-svm/svm_kernel/external/bootloader/target/x86_64-bootloader/debug/deps/rustc_std_workspace_core-a107913b465dbb04.rmeta: /home/lhebendanz/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/home/lhebendanz/Projects/rust-kernel-svm/svm_kernel/external/bootloader/target/x86_64-bootloader/debug/deps/librustc_std_workspace_core-a107913b465dbb04.rlib: /home/lhebendanz/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/home/lhebendanz/Projects/rust-kernel-svm/svm_kernel/external/bootloader/target/x86_64-bootloader/debug/deps/rustc_std_workspace_core-a107913b465dbb04.d: /home/lhebendanz/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/home/lhebendanz/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs:
