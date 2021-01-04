afc0782fc1a9251916b007e760f07c523f756412
Luis Hebendanz <luis.nixos@gmail.com>
2021-01-04 21:04:31 +0100

    Working hello World over serial in bootloader

--- svm_kernel/external/bootloader/src/main.rs
+++ svm_kernel/external/bootloader/src/main.rs
@@ -5,17 +5,22 @@
 #[allow(unused_imports)]
 use bootloader::*;
 
+use uart_16550::SerialPort;
 
 global_asm!(include_str!("boot.s"));
 
 extern "C" {
     static _kernel_start_addr: usize;
 }
-
+use core::fmt::Write;
 #[no_mangle]
 unsafe fn bootloader_main() {
     let ptr = 0xb8000 as *mut u32;
     *ptr = 0x2f4b2f4f + _kernel_start_addr as u32;
+
+    let mut serial_port = SerialPort::new(0x3F8);
+    serial_port.init();
+    serial_port.write_str("Hello World!").unwrap();
     loop {}
 }
 
