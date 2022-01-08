8af6e8ae8bc8e0040ccc5416bd9e721dc6a462f7
Luis-Hebendanz <luis.nixos@gmail.com>
2022-01-08 19:12:52 +0100

    Fixed tools

--- tools/gdb.sh
+++ tools/gdb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-gdb -ex "target remote: 1234" -ex "symbol-file ~/Projects/svm_kernel/kernel/target/x86_64-os/debug/perf_kernel"
- # -ex "symbol-file ~/Projects/svm_kernel/kernel/target/x86_64-os/debug/isofiles-perf_kernel/boot/kernel.elf"
+gdb -ex "target remote: 1234" -ex "symbol-file kernel/target/x86_64-os/debug/perf_kernel"
+ # -ex "symbol-file kernel/target/x86_64-os/debug/isofiles-perf_kernel/boot/kernel.elf"
