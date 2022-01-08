b45784acca28f052656a0b956793d15e14de0453
Luis-Hebendanz <luis.nixos@gmail.com>
2022-01-08 19:07:34 +0100

    Added lib workspaes

--- crates/perf_test/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub fn mix_mono_to_stereo(dst: &mut [f32], src: &[f32], gain_l: f32, gain_r: f32) {
-    for i in 0..src.len() {
-        dst[i * 2 + 0] = src[i] * gain_l;
-        dst[i * 2 + 1] = src[i] * gain_r;
-    }
-}
