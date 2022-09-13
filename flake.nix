6cd25232282d4d6bdfa23afd690aed741c6521ac
Luis-Hebendanz <luis.nixos@gmail.com>
2022-09-14 01:13:13 +0200

    Trying to fix nix build

--- flake.nix
+++ flake.nix
@@ -31,6 +31,10 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nci = {
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -38,9 +42,10 @@
     };
   };
 
-  outputs = { self, nixpkgs, nci, rust-overlay, glue-gun, nix-ipxe, nix-parse-gdt, vmsh-flake, flake-utils, nixos-codium, ... }:
+  outputs = { self, nixpkgs, naersk, nci, rust-overlay, glue-gun, nix-ipxe, nix-parse-gdt, vmsh-flake, flake-utils, nixos-codium, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
+        naersk-lib = pkgs.callPackage naersk { };
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
@@ -67,38 +72,49 @@
           inherit pkgs;
           vscodeBaseDir = "/tmp/nixos-codium-perfkernel";
         };
+
+        buildDeps = with pkgs; [
+          myipxe
+          vmsh
+          glue_gun
+          parse-gdt
+          mycodium
+          myrust
+          evcxr # rust repl
+          cargo-tarpaulin # for code coverage
+          rust-analyzer
+          zlib.out
+          xorriso
+          dhcp
+          grub2
+          qemu
+          entr # bash file change detector
+          #glibc.dev # Creates problems with tracy
+          netcat-gnu
+          git-extras
+          python3
+        ] ++ (with pkgs.python39Packages; [
+          pyelftools
+          intervaltree
+        ]) ++ (with pkgs.llvmPackages_latest; [
+          lld
+          bintools
+          llvm
+        ]);
       in
-      {
+      rec {
+        packages.default = naersk-lib.buildPackage {
+          src = ./kernel;
+          buildInputs = buildDeps;
+          root = ./kernel;
+          #remapPathPrefix = false;
+          #singleStep = true;
+        };
+        defaultPackage = packages.default;
+
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            myipxe
-            vmsh
-            glue_gun
-            parse-gdt
-            mycodium
-            myrust
-            evcxr # rust repl
-            cargo-tarpaulin # for code coverage
-            rust-analyzer
-            zlib.out
-            xorriso
-            dhcp
-            grub2
-            qemu
-            entr # bash file change detector
-            #glibc.dev # Creates problems with tracy
-            netcat-gnu
-            git-extras
-            python3
-          ] ++ (with pkgs.python39Packages; [
-            pyelftools
-            intervaltree
-          ]) ++ (with pkgs.llvmPackages_latest; [
-            lld
-            bintools
-            llvm
-          ]);
-          
+          buildInputs = buildDeps;
+
           IPXE = myipxe;
 
           LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
